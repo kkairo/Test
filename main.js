4edc6b1456c2deff6fde77a0c4a0be151284d870
Kerli Käiro <kerlikairo@Kerlis-MacBook-Air.local>
2018-06-11 08:45:04 +0300

    js

---
+++ main.js
@@ -0,0 +1,9 @@
+function showMenuIcon() {
+    var x = document.getElementById("navbar-links");
+    if (x.className === "navbar") {
+      x.style.
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
